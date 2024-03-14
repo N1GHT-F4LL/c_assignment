cb80e99b300b1c1a5d55f0c978de7a6c5dee00b1
Dang Minh Tri <60804710+N1GHT-F4LL@users.noreply.github.com>
2024-03-15 00:24:25 +0700

    Create problem_d.c

---
+++ problem_d.c
@@ -0,0 +1,28 @@
+# Problem D
+### Points: 10
+
+## Description
+
+
+On a small island with no internet connection, mobile phones, or modern means of communication, the inhabitants rely solely on a radio station to receive information from the outside world.
+
+One day, while everyone was busy with their daily tasks, the radio station suddenly broadcasted an urgent message. However, due to unstable signals, the message was repeated multiple times, forming a string of characters that everyone could hear.
+
+To understand the content of the message clearly, the islanders needed to determine the shortest possible string that could be the required information. Therefore, they needed a way to extract the message from this repeated string.
+
+You are tasked with helping the islanders by writing a program that can determine the length of the shortest string containing the required message.
+  
+## Input
+Message string. The string contains only characters from a..z.
+
+## Output
+
+The length of the substring that can be the actual message must be the smallest possible value
+
+## Requirements
+This problem only allow file name with "c" file extension "problemd.c", another name will be ignored.
+
+## Example
+| Input | Output |
+| ----- | ------ |
+|cabcabca|3|
